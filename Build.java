ca7a17be1904244ec36b6368c20b6a8dcd36a0ed
Umberto Raimondi <me@uraimo.com>
2016-02-17 22:40:49 +0100

    Added javabuild Build.java

---
+++ Build.java
@@ -0,0 +1,9 @@
+import com.uraimo.javabuild.*
+
+public class Build {
+    public SrcModule origin = new SrcModule(
+                        "JavaBuild",
+                        "0.0.1",
+                        new Module[]{
+                        });
+}
